707d7b5a3bd3730258a7ffeebabe5f4b19ecee71
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-06-08 05:24:42 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYeaAsFZuP2p7zwKPpu30DoFZa0E62h3Q",
+    authDomain: "quickcbt-f3e52.firebaseapp.com",
+    projectId: "quickcbt-f3e52",
+    storageBucket: "quickcbt-f3e52.firebasestorage.app",
+    messagingSenderId: "84415489295",
+    appId: "1:84415489295:web:150ca254278efaa8b20a7a",
+    measurementId: "G-JTDXD1SRPH"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
